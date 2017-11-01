6f93c810f9952b96eb7678762d0afbcdcbdb8916
Leandro Silva <leandro@leandrosilva.info>
2017-11-01 10:13:04 -0200

    Adding option to require a version via path

--- README.md
+++ README.md
@@ -42,8 +42,14 @@ const router = new Router();
 // Defines a version 1.0.0 for todo list
 router.get('todo.list', '/todo', api.version({'1.0.0': list}));
 
-// Defines 2 versions (order is not important)
+// Defines 2 versions (order is NOT important)
 router.get('todo.detail', '/todo/:id', api.version({
+    '1.1.0': detail,
+    '2.1.3': detail2
+}))
+
+// You can specify a route param as version. Will accept both /todo/321 and /v1/todo/321 
+router.get('todo.detail', '/:version(v\\d)?/todo/:id', api.version({
     '1.1.0': detail,
     '2.1.3': detail2
 }))
@@ -72,9 +78,25 @@ X-Api-Version: 1.1.0
 
 {"todo": {}}
 
+# Specifying a version via path
+$ curl -i http://localhost:3000/v1/todo/1
+HTTP/1.1 200 Accepted
+X-Api-Version: 1.1.0
+<more headers>
+
+{"todo": {}}
+
 $ curl -i -H "Accept-Version: ~2" http://localhost:3000/todo/1
 HTTP/1.1 200 Accepted
 X-Api-Version: 2.1.3
+<more headers>
+
+{"todo": {}}
+
+# Path has higher priority than header
+$ curl -i -H "Accept-Version: ~2" http://localhost:3000/v1/todo/1
+HTTP/1.1 200 Accepted
+X-Api-Version: 1.1.0
 <more headers>
 
 {"todo": {}}
@@ -84,7 +106,7 @@ $ curl -H "Accept-Version: ^3.0" http://localhost:3000/todo/1
 HTTP/1.1 400 Bad Request
 <more headers>
 
-^3 version is not supported
+Version ^3 is not supported
 ```
 
 ### Version state variable
@@ -121,6 +143,31 @@ router.get('todo.list', '/todo', api.version(
 ));
 ```
 
+### routeParam
+
+This module expects the path as ```v``` followed by a single number:
+* v1 is equivalent to ^1
+* v2 is equivalent to ^2
+* v11 is equivalent to ^11
+
+The default param is ```version```, but you can change it:
+ 
+```js
+router.get('todo.list', '/:myversion(v\\d)/todo', api.version(
+  {'1.0.0': list},
+  { routeParam: 'myversion' }
+));
+```
+
+In order to make version optional and use the latest, use the ```?``` regex at the end:
+ 
+```js
+router.get('todo.list', '/:myversion(v\\d)?/todo', api.version(
+  {'1.0.0': list},
+  { routeParam: 'myversion' }
+));
+```
+ 
 ### fallbackLatest
 
 When the requested version is not found, the default response is an error, 
--- test/test.js
+++ test/test.js
@@ -25,6 +25,25 @@ describe('KoaRouterVersion', function() {
         '2.0.0': handler()
       }
     ));
+
+    router.get('path.list', '/:version(v\\d)?/path', api.version(
+      {
+        '1.1.0': handler(),
+        '1.0.0': handler(),
+        '2.0.0': handler()
+      }
+    ));
+
+    router.get('renamed.list', '/:renamed(v\\d)?/renamed', api.version(
+      {
+        '1.1.0': handler(),
+        '1.0.0': handler(),
+        '2.0.0': handler()
+      },
+      {
+        routeParam: 'renamed'
+      }
+    ));
     router.get('todo2.list', '/todo2', api.version({'1.0.0': handler()}));
     router.get('todo3.list', '/todo3', api.version({'1.3.0': handler()}, { fallbackLatest:true }));
     app.use(router.routes());
@@ -100,7 +119,7 @@ describe('KoaRouterVersion', function() {
       .set('Accept-Version', '^3.0')
       .end(function(err, res) {
         expect(res.statusCode).to.equal(400);
-        expect(res.text).to.equal('^3.0 version is not supported');
+        expect(res.text).to.equal('Version ^3.0 is not supported');
         done();
       });
   });
@@ -113,6 +132,74 @@ describe('KoaRouterVersion', function() {
       .end(function(err, res) {
         expect(res.statusCode).to.equal(200);
         expect(res.headers['x-api-version']).to.equal('1.3.0');
+        done();
+      });
+  });
+
+  it('should match a single version via path', function(done) {
+
+    request(server)
+      .get('/v1/path')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.headers['x-api-version']).to.equal('1.1.0');
+        done();
+      });
+  });
+
+  it('should match a single version via path', function(done) {
+
+    request(server)
+      .get('/v2/path')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.headers['x-api-version']).to.equal('2.0.0');
+        done();
+      });
+  });
+
+  it('should match latest version via empty path', function(done) {
+
+    request(server)
+      .get('/path')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.headers['x-api-version']).to.equal('2.0.0');
+        done();
+      });
+  });
+
+
+  it('should choose path over header', function(done) {
+
+    request(server)
+      .get('/v2/path')
+      .set('Accept-Version', '^1.0')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.headers['x-api-version']).to.equal('2.0.0');
+        done();
+      });
+  });
+
+  it('should throw an error if requested an invalid version via path', function(done) {
+
+    request(server)
+      .get('/v3/path')
+      .end(function(err, res) {
+        expect(res.statusCode).to.equal(400);
+        expect(res.text).to.equal('Version 3 is not supported');
+        done();
+      });
+  });
+
+  it('should accept a renamed version path', function(done) {
+
+    request(server)
+      .get('/v1/renamed')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.headers['x-api-version']).to.equal('1.1.0');
         done();
       });
   });
